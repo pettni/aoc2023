15f7f3fa4c0e9cba1431490e7234f7afe98440d4
Petter Nilsson <pettni@umich.edu>
2023-12-21 13:28:52 -0500

    Speed up day 17

--- src/Day17.hs
+++ src/Day17.hs
@@ -2,10 +2,13 @@
 
 module Day17 (solve1, solve2) where
 
+import Control.Arrow ((>>>))
+import qualified Data.Array.Unboxed as A
 import Data.Char (ord)
 import qualified Data.Heap as H
 import qualified Data.Set as S
 import Data.Text (Text, unpack)
+import Data.Tuple (swap)
 import qualified Text.Parsec as Parsec
 
 -- Types
@@ -18,13 +21,18 @@ data State = State {pos :: Pos, dir :: Dir, stepsInDir :: Int} deriving (Eq, Ord
 
 data StateWithCost = StateWithCost {state :: State, cost :: Int} deriving (Eq, Ord)
 
+type Board = A.Array Pos Int
+
 -- Parsing
 
-parseInput :: Text -> [[Int]]
-parseInput ss = handleErr $ Parsec.parse inputData "Failure" $ unpack ss
+parseInput :: Text -> Board
+parseInput ss = toBoard $ handleErr $ Parsec.parse inputData "Failure" $ unpack ss
   where
     inputData = Parsec.sepEndBy1 intLine Parsec.endOfLine
     intLine = map (\x -> ord x - ord '0') <$> Parsec.many1 (Parsec.noneOf "\n")
+
+    toBoard xs = A.listArray ((0, 0), (length xs - 1, length (head xs) - 1)) (concat xs)
+
     handleErr (Left a) = error (show a)
     handleErr (Right a) = a
 
@@ -42,11 +50,10 @@ step (x, y) TN = (x, y - 1)
 step (x, y) TW = (x - 1, y)
 step (x, y) TE = (x + 1, y)
 
-pathFind :: [[Int]] -> Int -> Int -> State -> (State -> Bool) -> Int
+pathFind :: Board -> Int -> Int -> State -> (State -> Bool) -> Int
 pathFind board minStep maxStep init goalCond = pathFind' mempty (H.singleton (0, StateWithCost init 0))
   where
-    nRows = length board
-    nCols = length $ head board
+    (_, (yMax, xMax)) = A.bounds board
 
     pathFind' :: S.Set (Pos, Dir, Int) -> H.MinPrioHeap Int StateWithCost -> Int
     pathFind' close open = case H.view open of
@@ -62,29 +69,20 @@ pathFind board minStep maxStep init goalCond = pathFind' mempty (H.singleton (0,
               [ (newRunningCost + heuristic newState, StateWithCost newState newRunningCost)
                 | dirOut <- [TN, TS, TW, TE],
                   dirOut /= opposite dir,
-                  let posOut@(x', y') = step pos dirOut,
-                  y' >= 0,
-                  y' < length board,
-                  x' >= 0,
-                  x' < length (board !! y'),
+                  let posOut = step pos dirOut,
+                  A.inRange (A.bounds board) $ swap posOut,
                   let stepsInDirOut = if dir == dirOut then stepsInDir + 1 else 1,
                   (dirOut /= dir) || (stepsInDirOut <= maxStep),
                   (dirOut == dir) || (stepsInDir >= minStep),
                   let newState = State posOut dirOut stepsInDirOut,
-                  let newRunningCost = runningCost + board !! y' !! x'
+                  let newRunningCost = runningCost + board A.! swap posOut
               ]
-          heuristic (State (x, y) _ _) = abs (x - (nCols - 1)) + abs (y - (nRows - 1))
+          heuristic (State (x, y) _ _) = abs (x - xMax) + abs (y - yMax)
 
 solve :: Int -> Int -> Text -> Int
-solve minStep maxStep ss = minCost
+solve minStep maxStep ss = pathFind board minStep maxStep (State (0, 0) TE 0) (pos >>> swap >>> (== snd (A.bounds board)))
   where
     board = parseInput ss
-    minCost = pathFind board minStep maxStep (State (0, 0) TE 0) goalCond
-      where
-        goalCond (State (x, y) _ _) = x == nCols - 1 && y == nRows - 1
-          where
-            nRows = length board
-            nCols = length $ head board
 
 solve1 :: Text -> Int
 solve1 = solve 0 3
